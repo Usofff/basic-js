927f4e6bb468d866d8620b2bf9046e0c187e6ed7
Ilya Usov <iusov41@gmail.com>
2021-03-11 10:47:49 +0300

    transform-array

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,11 +1,12 @@
-
+const CustomError = require("../extensions/custom-error");
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  let obj = {
-    turns : 0,
-    seconds : 0
-};
-obj.turns = Math.pow(2,disksNumber)-1;
-obj.seconds = obj.turns*3600/turnsSpeed;
-obj.seconds = Math.floor(obj.seconds);
-return obj;
+  throw new CustomError('Not implemented');
+//   let obj = {
+//     turns : 0,
+//     seconds : 0
+// };
+// obj.turns = Math.pow(2,disksNumber)-1;
+// obj.seconds = obj.turns*3600/turnsSpeed;
+// obj.seconds = Math.floor(obj.seconds);
+// return obj;
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,29 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+  if(!(Array.isArray(arr))){
+    
+    throw new Error('Error');
+  } 
+  let a = arr.slice();
+  if(a[0] === '--discard-prev' || a[0] === '--double-prev'){
+    a.shift();
+  } 
+  if(a[a.length - 1] === '--discard-next' || a[a.length - 1] === '--double-next'){
+    a.pop();
+  } 
+  for(let i = 0; i < a.length; i++){
+    if (a[i].toString() === '--discard-next')
+    {
+      a.splice(i, 2, "deleted");
+    }else if(a[i].toString() === '--discard-prev'){
+      a.splice(i - 1, 2, "deleted");
+    }else if(a[i].toString() === '--double-next'){
+      a.splice(i, 1, a[i + 1]);
+    }else if(a[i].toString() === '--double-prev'){
+      a.splice(i, 1, a[i - 1]);
+    } 
+  }
+  return a.filter(e => e != 'deleted');
+
 };

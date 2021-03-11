eeb5da6ae8e45c5131a53287cf3e9ccab32979d6
Ilya Usov <iusov41@gmail.com>
2021-03-11 10:54:21 +0300

    transform-array

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 module.exports = function countCats(cats) {
-  throw new CustomError('Not implemented');
   let c=0;
   cats.forEach(function (e) {
     e.forEach(element => {

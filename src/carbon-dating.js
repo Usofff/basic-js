4445bb7b467e889fd9d53a94265698aaece893b1
Ilya Usov <iusov41@gmail.com>
2021-03-11 10:58:17 +0300

    fix carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,8 +2,7 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 const CustomError = require("../extensions/custom-error");
 module.exports = function dateSample(sampleActivity) {
-  if (typeof sampleActivity == String) {
-
+  if (typeof sampleActivity === String) {
     let t = Math.log(MODERN_ACTIVITY / +sampleActivity) / 0.693 * HALF_LIFE_PERIOD;
   return Math.ceil(t);
   }else{

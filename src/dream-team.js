a57473bcfe35de08d4390977e3c6b22b8b2a06da
Ilya Usov <iusov41@gmail.com>
2025-01-05 16:30:41 +0300

    feat: implement tasks: count cats, dream-team, what-season

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam( members) {
+  if(!Array.isArray(members)) return false;
+  const validMembers = members.filter(el => typeof el == 'string')
+  if(validMembers.length === 0) {
+    return false
+  }
+  return validMembers.map(el => el.trim().toUpperCase()[0]).sort().join('')
 }
 
 module.exports = {
